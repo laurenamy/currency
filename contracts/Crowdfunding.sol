d14e68e8e05f8c38e0b98ca7d3cf3f5adc61ba31
Lauren Adam <lauren.a.adam@hotmail.com>
2019-08-21 14:43:14 -0400

    wip: crowdfunding

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./Token.sol";
 
 contract Crowdfunding {
+  using SafeMath for uint;
   struct Project {
     string title;
     uint tokenGoal;
@@ -20,6 +22,7 @@ contract Crowdfunding {
   Project[] public projects;
   uint[] public projectIds;
   Token public token;
+  mapping (uint => uint256) funds;
 
   /***************
   EVENTS
@@ -27,6 +30,15 @@ contract Crowdfunding {
   event ProjectCreated (
     uint projectId
   );
+
+  /***************
+  MODIFIERS
+  ***************/
+
+  modifier whenNoProjectIds() {
+    require(projectIds.length > 0, "There are no project ids to return");
+    _;
+  }
 
   /********
   FUNCTIONS
@@ -36,6 +48,7 @@ contract Crowdfunding {
     Project memory _project = Project({title: _title, tokenGoal: _tokenGoal, duration: _duration});
     uint projectId = projects.push(_project) - 1;
     projectIds.push(projectId);
+    funds[projectId] = 0;
     emit ProjectCreated(projectId);
   }
   function getProject(uint _projectId) public view returns (string memory title, uint tokenGoal, uint duration) {
@@ -46,7 +59,18 @@ contract Crowdfunding {
     duration = _project.duration;
   }
 
-  function getProjectIds() public view returns (uint[] memory) {
+  function getProjectIds() public view whenNoProjectIds returns (uint[] memory) {
     return projectIds;
+  }
+
+  function makeDonation(uint _projectId, uint _tokens) public payable whenNoProjectIds {
+    require(token.balanceOf(msg.sender) >= _tokens, 'Must have enough tokens to process transaction');
+    require(_tokens > 0, 'Amount must not be zero');
+    token.transferFrom(msg.sender, _tokens);
+    funds[_projectId].add(_tokens);
+  }
+
+  function getFunds(uint _projectId) public returns (uint) {
+    return funds[_projectId];
   }
 }
--- test/Crowdfunding.test.js
+++ test/Crowdfunding.test.js
@@ -7,13 +7,15 @@ const web3 = new Web3(Web3.givenProvider);
 
 contract('Crowdfunding', function (accounts) {
   const owner = accounts[0];
+  const donor = accounts[1]
   const title = "my project";
   const tokenGoal = new BN(1000);
   const duration = new BN(10);
   const startDate = 1533114000;
   const endDate = 1596272400;
   const testTime = 1563114000;
-  const tokenSupply = new BN(50);
+  const tokenSupply = new BN(500);
+  const amountTokens = new BN(100);
 
   beforeEach(async function () {
     token = await Token.new(startDate, endDate, tokenSupply);
@@ -25,9 +27,14 @@ contract('Crowdfunding', function (accounts) {
     crowdfund = await Crowdfunding.new(tokenAddress);
     crowdfundAddress = crowdfund.address;
     crowdfundInstance = await Crowdfunding.at(crowdfundAddress);
+
+    await token.approve(crowdfundAddress, tokenGoal);
+    await token.approve(owner, amountTokens);
+    await token.transferFrom(donor, amountTokens, { from: owner });
+    
   });
   describe('createProject', function () {
-    it('creates a project with the correct attributes', async function () {
+    it('should create a project with the correct attributes', async function () {
       let { logs } = await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
       let projectId = logs[0].args.projectId.toNumber();
       let project = await crowdfund.getProject(projectId);
@@ -35,13 +42,53 @@ contract('Crowdfunding', function (accounts) {
       assert.equal(project.tokenGoal, tokenGoal.toNumber());
       assert.equal(project.duration, duration.toNumber());
     });
-    it('reverts if the project owner does not have any tokens', async function () {
+    it('should revert if the project owner does not have any tokens', async function () {
       await expectRevert(crowdfund.createProject(title, tokenGoal, duration, { from: accounts[2] }), 'User must hold tokens to create a project');
     });
   });
   describe('getProjectIds', function () {
-    it('returns an array of the correct length', async function () {
+    it('should return an array of the correct length', async function () {
+      await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      let projectIds = await crowdfund.getProjectIds();
+      assert.equal(projectIds.length, 3);
+    });
+    it('should revert if there are no project ids', async function () {
+      await expectRevert(crowdfund.getProjectIds(), 'There are no project ids to return');
+    });
+  });
+  describe('makeDonation', function () {
+    it('should revert if the balance of the donor is insufficient', async function () {
+      let { logs } = await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      let projectId = logs[0].args.projectId.toNumber();
+        
+      await expectRevert(crowdfund.makeDonation(projectId, tokenSupply, { from: donor }), 'Must have enough tokens to process transaction');
+    });
+    it('should revert if the amount of tokens is zero', async function () {
+      let { logs } = await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      let projectId = logs[0].args.projectId.toNumber();
 
+      await expectRevert(crowdfund.makeDonation(projectId, 0, { from: donor }), 'Amount must not be zero');
+    });
+    it('should increase the project balance by the correct amount of tokens', async function () {
+
+    });
+    it('should decrease the donor balance by the correct amount of tokens', async function () {
+      let { logs } = await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      let projectId = logs[0].args.projectId.toNumber();
+      let previousBalance = await token.balanceOf(donor);
+      await crowdfund.makeDonation(projectId, amountTokens.toNumber(), { from: donor });
+      let newBalance = await token.balanceOf(donor);
+      assert.equal(newBalance - amountTokens, previousBalance);
+    });
+    it('should increase the amount of tokens a project has', async function () {
+      let { logs } = await crowdfund.createProject(title, tokenGoal, duration, { from: owner });
+      let projectId = logs[0].args.projectId.toNumber();
+      await crowdfund.makeDonation(projectId, amountTokens.toNumber(), { from: donor });
+            
+      let a = await crowdfund.funds[projectId].call();
+      console.dir(a)
     });
   });
 });
