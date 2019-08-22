de54b5a1efe4926af67f5868580971bb0980403b
Lauren Adam <lauren.a.adam@hotmail.com>
2019-08-22 13:32:34 -0400

    feat: added comments

--- contracts/Contribution.sol
+++ contracts/Contribution.sol
@@ -1,8 +1,5 @@
 pragma solidity ^0.5.0;
 
-// Users can donate ETH to Contribution contract
-// Users receive Tokens in exchange for their ETH from the Contribution Contract
-
 import "./Token.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
@@ -10,17 +7,30 @@ import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 contract Contribution is Pausable {
   using SafeMath for uint;
 
-  uint256 private rate = 10**18;
-  Token public tokenAddress;
+  /****************
+  GLOBAL CONSTANTS
+  *****************/
+  uint256 private rate = 10**18; // conversation rate
+  Token public tokenAddress;  // token instance
+  mapping(address => uint256) public donations; //
 
+  /****************
+  EVENTS
+  *****************/
   event Sent(address from, uint256 value);
 
+
+  /****************
+  FUNCTIONS
+  *****************/
   constructor (address _tokenAddress) public {
     tokenAddress = Token(_tokenAddress);
   }
 
-  mapping(address => uint256) public donations;
-
+  /**
+  * @dev Sends a contribution to the contract and calls _sendTokens
+  * in return
+  */
   function sendContribution() public payable whenNotPaused {
     require(msg.value > 0, "Amount must not be zero.");
     _sendTokens(msg.sender, msg.value);
@@ -28,11 +38,20 @@ contract Contribution is Pausable {
     emit Sent(msg.sender, msg.value);
   }
 
+  /**
+  * @dev Returns all contributions from the given address
+  * @param _donor The address of the donor
+  */
   function getContributions(address _donor) public view returns (uint256) {
     require(donations[_donor] != 0, "No donations from given address.");
     return donations[_donor];
   }
 
+  /**
+  * @dev Sends the appropriate amount of tokens given the amount of wei
+  * @param recipient Account to receive the tokens
+  * @param amountWei Amount of wei to be converted to tokens
+  */
   function _sendTokens(address recipient, uint256 amountWei) internal {
     uint numTokens = amountWei.div(rate);
     require(tokenAddress.totalSupply() >= numTokens, "Insufficient amount of tokens.");
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,17 +4,33 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract Token is Ownable, ERC20 {
-  uint40 public startTime;
-  uint40 public endTime;
+  /****************
+  GLOBAL CONSTANTS
+  *****************/
+  uint40 public startTime; // start of when tokens can be purchased
+  uint40 public endTime;  // end of when tokens can be purchased
 
+  /****************
+  EVENTS
+  *****************/
   event UpdatedTime(uint40 time);
 
+  /****************
+  FUNCTIONS
+  *****************/
   constructor(uint40 _startTime, uint40 _endTime, uint256 initialSupply) public {
     _mint(msg.sender, initialSupply);
     startTime = _startTime;
     endTime = _endTime;
   }
 
+  // TOKEN STATE //
+
+  /**
+  * @dev Transfers tokens from an account to another account
+  * @param recipient The account receiving the tokens
+  * @param amount The amount of tokens to be transfered
+  */
   function transferFrom(address recipient, uint256 amount) public returns (bool) {
     require(now >= startTime && now < endTime, "Must take place within the given time window");
     address owner = this.owner();
@@ -22,11 +38,21 @@ contract Token is Ownable, ERC20 {
     return true;
   }
 
+  /**
+  * @dev Sets the startTime for when tokens can begin to be transferred
+  * @param startDate The time to set the startTime to
+  * @param
+  */
   function setStartDate(uint40 startDate) public onlyOwner {
     startTime = startDate;
     emit UpdatedTime(startTime);
   }
 
+  /**
+  * @dev Sets the endTime for when tokens can no longer be transferred
+  * @param endDate The time to set the endTime to
+  * @param
+  */
   function setEndDate(uint40 endDate) public onlyOwner {
     endTime = endDate;
     emit UpdatedTime(endTime);
